f4ece3c07776a992f04b77be9ebea0c6a0e9ee38
Moses mpande <www.mosesmpande@gmail.com>
2024-05-11 14:50:35 +0200

    Initial Commit

---
+++ Unka Drivers/Unka_DriversApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unka_DriversApp.swift
+//  Unka Drivers
+//
+//  Created by Moses Mpande on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Unka_DriversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Unka DriversUITests/Unka_DriversUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Unka_DriversUITestsLaunchTests.swift
+//  Unka DriversUITests
+//
+//  Created by Moses Mpande on 11/05/2024.
+//
+
+import XCTest
+
+final class Unka_DriversUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
